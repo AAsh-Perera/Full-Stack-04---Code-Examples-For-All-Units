34df14ffb66f2760b42e40f8d8a56cf7c5f747c8
Ashane Perera (Wazda, the great penguin) <ashane.me12@gmail.com>
2024-07-17 14:32:46 +0530

    var 0.0.2 - Handlebars bug was fixed

--- units/Unit-20-template-engines/app.js
+++ units/Unit-20-template-engines/app.js
@@ -5,20 +5,21 @@ const port = 3001;
 
 const app = express();
 
-// now set up an instance of express-handlebars as the engine
-const hbs = exphbs.create();
-
-app.engine('handlebars', hbs.engine);
-//let's set the view engine to view all files ending with handlebars aka .hbs files
-app.set('view engine', 'handlebars');
+// Set up an instance of express-handlebars as the engine
+const hbs = exphbs.create({
+    defaultLayout: false
+});
 app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', hbs.engine);
+// Let's set the view engine to view all files ending with handlebars aka .hbs files
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    console.log('Request revieved for "/"');
-    res.render('main');
+    console.log('Request received for "/"');
+    res.render('index');
 });
 
-app.listen(port, ()=> {
-    console.log('Exress Server is running on port: ', port);
+app.listen(port, () => {
+    console.log('Express Server is running on port:', port);
     console.log(__dirname);
 });
