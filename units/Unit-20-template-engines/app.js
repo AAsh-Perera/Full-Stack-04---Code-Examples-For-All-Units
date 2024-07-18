77fb956fade1d64f7a16d2610e7488cba69e0794
Ashane Perera (Wazda, the great penguin) <ashane.me12@gmail.com>
2024-07-18 13:48:03 +0530

    Ver 1.0.0 - Code examples for all Units in Module 04

--- units/Unit-20-template-engines/app.js
+++ units/Unit-20-template-engines/app.js
@@ -7,7 +7,9 @@ const app = express();
 
 // Set up an instance of express-handlebars as the engine
 const hbs = exphbs.create({
-    defaultLayout: false
+    // what this does it make it so that the there is no default template.
+    // but even thoigh we are not using a default layout/template, we still need to have a main.hbs in a dir named 'layouts' with in the views dir.
+    defaultLayout: 'main'
 });
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', hbs.engine);
@@ -16,6 +18,7 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     console.log('Request received for "/"');
+    // remeber for this to work, you also need the main.hbs file in the 'layouts' dir which should be in the 'views' dir.
     res.render('index');
 });
 
